cdf1f3a472bd0c3248e744d491c641d8b20f67e9
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-03-27 22:15:51 +0600

    display card successfuly

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Products from '../ShowProducts/Products';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -11,14 +12,19 @@ const Shop = () => {
         loadData()
     },[])
     return (
-        <div className="grid grid-cols-5 min-h-[100vh]">
-            <div className="col-span-4 w-full">
+        <>
+            <h3 className="text-base font-semibold pl-5 pt-5">Products({products.length})</h3>
+            <div className="grid grid-cols-5 min-h-[100vh]">
+                <div className="col-span-4 w-full grid grid-cols-2 sm:grid-cols-4 gap-6 sm:px-5 py-3">
+                    {
+                        products.map(product => <Products key={product.id} product={product}></Products>)
+                    }
+                </div>
+                <div className="bg-[#FF9900] w-full">
 
+                </div>
             </div>
-            <div className="bg-[#FF9900] w-full">
-
-            </div>
-        </div>
+        </>
     );
 };
 
--- src/components/ShowProducts/Products.jsx
+++ src/components/ShowProducts/Products.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Products = () => {
+const Products = (props) => {
+    console.log(props.product);
+    const {img, name, price, rating, ratingCount, seller, shipping, stock} = props.product;
     return (
-        <div>
-            
+        <div className="p-1 border-2 rounded-md  flex flex-col justify-between">
+            <img className="w-full rounded" src={img} alt="" />
+            <div className="p-2">
+                <h4 className="text-base font-semibold">{name}</h4>
+                <p className="text-sm font-semibold mb-2">Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {rating}</p>
+                <p>Stock Status: {stock}</p>
+            </div>
+            <div className="h-10 flex items-center justify-center bg-[#FFE0B3]">
+                <p>Add to Cart</p>
+            </div>
         </div>
     );
 };
