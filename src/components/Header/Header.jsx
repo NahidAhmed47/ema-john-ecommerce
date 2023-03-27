c979920819e3046f18c57b483534fb31a2f642c4
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-03-27 21:12:30 +0600

    header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../assets/images/Logo.svg';
+const Header = () => {
+    return (
+        <div className="bg-[#1C2B35] py-2 flex justify-between items-center sm:px-12">
+            <img src={logo} alt="" />
+            <div className="text-white flex gap-6 font-semibold">
+                <a href="/order">Order</a>
+                <a href="/order_review">Order Review</a>
+                <a href="/manage_inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
