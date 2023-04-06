bdb441d202c860149db591eaab86801a37b2040f
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-04-06 20:00:14 +0600

    react-router install and route setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '../../assets/images/Logo.svg';
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className="bg-[#1C2B35] py-2 flex justify-between items-center sm:px-12">
-            <img src={logo} alt="" />
+            <Link to="/"><img src={logo} alt="" /></Link>
             <div className="text-white flex gap-6 font-semibold">
-                <a href="/shop">Shop</a>
-                <a href="/order_review">Order Review</a>
-                <a href="/manage_inventory">Manage Inventory</a>
-                <a href="/login">Login</a>
+                <Link to="/shop">Shop</Link>
+                <Link to="orders">Orders</Link>
+                <Link to="inventory">Inventory</Link>
+                <Link to="login">Login</Link>
             </div>
         </div>
     );
---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            This is order page
+        </div>
+    );
+};
+
+export default Orders;
